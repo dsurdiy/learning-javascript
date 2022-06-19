90c9dffcf3438d36a444c214ee89da122b1a7912
DimaSurdiy <dsurdiy@ukr.net>
2022-06-19 19:44:22 +0300

    Update 02-interval.js

--- src/js/02-interval.js
+++ src/js/02-interval.js
@@ -1,1 +1,24 @@
 import '../css/common.css';
+
+/*
+ * Метод setInterval(callback, delay, args)
+ */
+
+const logger = time => console.log(`Лог каждые ${time}ms - ${Date.now()}`);
+
+// console.log('Лог до setInterval');
+// setInterval(logger, 2000, 2000);
+// console.log('Лог после setInterval');
+
+/*
+ * Очистка интервала с clearInterval(intervalId)
+ */
+
+const intervalId = setInterval(logger, 2000, 2000);
+
+const shouldCancelInterval = Math.random() > 0.5;
+console.log(shouldCancelInterval);
+
+if (shouldCancelInterval) {
+  clearInterval(intervalId);
+}
