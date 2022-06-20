cbdc321f622f6e0d2f590feba794e9afe8167024
DimaSurdiy <dsurdiy@ukr.net>
2022-06-20 06:33:07 +0300

    Update 04-subscription

--- src/js/04-subscription.js
+++ src/js/04-subscription.js
@@ -1,2 +1,35 @@
 import '../css/common.css';
 import BSN from 'bootstrap.native';
+
+const refs = {
+  modal: document.querySelector('#subscription-modal'),
+  subscribeBtn: document.querySelector('button[data-subscribe]'),
+};
+
+const PROMPT_DELAY = 1000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+const modal = new BSN.Modal('#subscription-modal');
+
+openModal();
+
+refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
+refs.modal.addEventListener('hide.bs.modal', openModal);
+
+function openModal() {
+  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+    return;
+  }
+
+  setTimeout(() => {
+    console.log('Открываем надоедалку');
+    modal.show();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
+}
+
+function onSubscribeBtnClick() {
+  hasSubscribed = true;
+  modal.hide();
+}
