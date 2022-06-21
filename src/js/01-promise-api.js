6186a2190598da1ca6d53a762bfd0f6e0f6eddd9
DimaSurdiy <dsurdiy@ukr.net>
2022-06-21 20:56:11 +0300

    Add new README and update 01-promise-api.js

---
+++ src/js/01-promise-api.js
@@ -0,0 +1,45 @@
+/*
+ * Создание промиса
+ *  - Класс Promise
+ *  - resolve
+ *  - reject
+ *  - Promise.prototype.then(onResolve, onReject)
+ */
+
+const promise = new Promise((resolve, reject) => {
+  const isFulfill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (isFulfill) {
+      resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+    }
+
+    reject('Промис выполнился с ошибкой (отклонён, rejected)');
+  }, 1000);
+});
+
+// promise.then(onFulfilled, onRejected);
+
+function onFulfilled(result) {
+  console.log(`✅ ${result}`);
+}
+
+function onRejected(error) {
+  console.log(`❌ ${error}`);
+}
+
+/*
+ * Цепочки промисов (chaining)
+ * Promise.prototype.catch(error)
+ * Promise.prototype.finally()
+ */
+promise
+  .then(onFulfilled)
+  .then(x => {
+    console.log(x);
+
+    return 10;
+  })
+  .then(y => console.log(y))
+  .catch(error => console.log(error))
+  .finally(() => console.log('Я буду выполнен в любом случае'));
