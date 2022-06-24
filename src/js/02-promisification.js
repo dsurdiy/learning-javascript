17b553e69267800cec508117164db094a6017f6c
DimaSurdiy <dsurdiy@ukr.net>
2022-06-24 21:04:19 +0300

    Update 02-promisification.js

--- src/js/02-promisification.js
+++ src/js/02-promisification.js
@@ -27,20 +27,55 @@
  * - Promise.resolve()
  * - Promise.reject()
  */
-const makeOrder = (dish, onSuccess, onError) => {
-  const passed = Math.random() > 0.5;
 
-  if (passed) {
-    onSuccess(`✅ Вот ваш заказ: ${dish}`);
-  }
+// const makeOrder = dish => {
+//   return Promise.resolve(`✅ Вот ваш заказ: ${dish}`);
+// };
 
-  onError('❌ Упс, у нас закончились продукты');
+// makeOrder('пирожок').then(onMakeOrderSuccess);
+
+// function onMakeOrderSuccess(result) {
+//   console.log(result);
+// }
+
+// function onMakeOrderError(error) {
+//   console.log(error);
+// }
+
+/*
+ * Покемоны с https://pokeapi.co/
+ */
+
+// const fetchPokemonById = id => {
+//   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r => r.json());
+// };
+
+// fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
+
+// fetchPokemonById(2).then(onFetchSuccess).catch(onFetchError);
+
+// function onFetchSuccess(pokemon) {
+//   console.log(pokemon);
+// }
+
+// function onFetchError(error) {
+//   console.log(error);
+// }
+
+const makePromise = () => {
+  return new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        resolve('✅ Все ок, это resolve');
+      }
+
+      reject('❌ Это reject');
+    }, 2000);
+  });
 };
 
-function onMakeOrderSuccess(result) {
-  console.log(result);
-}
-
-function onMakeOrderError(error) {
-  console.log(error);
-}
+makePromise()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
