dfeee2236b4e42144766a39f786440319024d75f
DimaSurdiy <dsurdiy@ukr.net>
2022-06-26 16:24:51 +0300

    Update 02-cors.js

--- src/js/02-cors.js
+++ src/js/02-cors.js
@@ -7,3 +7,9 @@
  *  - https://lpj-weather-service.herokuapp.com/
  */
 import '../css/common.css';
+
+fetch(
+  'https://lpj-weather-service.herokuapp.com/weather?lat=39.916668&lon=116.383331',
+)
+  .then(r => r.json())
+  .then(console.log);
