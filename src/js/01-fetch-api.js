b55a3ce8de7593ed4e266a11ce0834edadd5e24c
DimaSurdiy <dsurdiy@ukr.net>
2022-06-26 10:36:42 +0300

    Update 01-fetch-api.js

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -12,3 +12,32 @@
  * - Библиотеки-обёртки
  * - https://pokeapi.co/
  */
+import '../css/common.css';
+import pokemonCardTpl from '../templates/pokemon-card.hbs';
+import API from './api-service';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  API.fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Что-то пошло не так!');
+}
