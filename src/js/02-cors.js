6cf8a36e3ed5f10c7af39dea2713aca8c09ff6b4
DimaSurdiy <dsurdiy@ukr.net>
2022-06-26 07:44:23 +0300

    Add starting files for lesson-19

---
+++ src/js/02-cors.js
@@ -0,0 +1,9 @@
+/*
+ * - Cross-Origin Resource Sharing (CORS)
+ *  - https://pokeapi.co/
+ *  - https://darksky.net/dev
+ * - Proxy
+ *  - Хранение API ключей и секретов
+ *  - https://lpj-weather-service.herokuapp.com/
+ */
+import '../css/common.css';
