e379e89ef13558a7d7a42f81a58560a559111b26
DimaSurdiy <dsurdiy@ukr.net>
2022-06-29 05:45:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,47 @@
  * - Сброс значения при поиске по новому критерию
  *
  * https://newsapi.org/
- * 4330ebfabc654a6992c2aa792f3173a3
  * http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
  */
 
 import './css/common.css';
+import NewsApiService from './js/components/news-service';
+import articlesTpl from './templates/articles.hbs';
+
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  articlesContainer: document.querySelector('.js-articles-container'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  newsApiService.query = e.currentTarget.query.value;
+
+  if (newsApiService.query === '') {
+    return alert('Введи что-то нормальное');
+  }
+
+  newsApiService.resetPage();
+  newsApiService.fetchArticles().then(articles => {
+    clearArticlesContainer();
+    appendArticlesMarkup(articles);
+  });
+}
+
+function onLoadMore() {
+  newsApiService.fetchArticles().then(appendArticlesMarkup);
+}
+
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
+
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
