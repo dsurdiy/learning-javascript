ed7257b4d4b315b7fa6888048e9f1384d79c6dfd
DimaSurdiy <dsurdiy@ukr.net>
2022-07-10 06:53:51 +0300

    Update 03-async.js

--- src/js/03-async.js
+++ src/js/03-async.js
@@ -2,3 +2,50 @@
 // Последовательные операции
 // Паралельные операции с Promise.all()
 // try...catch
+
+function getFruit(name) {
+  const fruits = {
+    strawberry: '🍓',
+    kiwi: '🥝 ',
+    apple: '🍎',
+  };
+
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve(fruits[name]), 500),
+  );
+}
+
+async function aMakeSmoothie() {
+  try {
+    console.time('aMakeSmoothie');
+
+    const apple = getFruit('apple');
+    const kiwi = getFruit('kiwi');
+    const berry = getFruit('strawberry');
+
+    const fruits = await Promise.all([apple, kiwi, berry]);
+    console.log(fruits);
+
+    console.timeEnd('aMakeSmoothie');
+
+    return fruits;
+  } catch (error) {
+    console.log('Ошибка');
+  }
+}
+
+aMakeSmoothie().then(fruits => console.log(fruits));
+
+// async function foo() {}
+
+// const x = async function () {};
+
+// const fn = async () => {};
+
+// const r = {
+//   async getName() {},
+// };
+
+// class X {
+//   async getHero() {}
+// }
